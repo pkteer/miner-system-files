e6c6848e0201586ef4a280758e4e67a560c77c11
Caleb James DeLisle <cjd@cjdns.fr>
2021-02-25 05:16:08 +0100

    use src, not via

--- scripts/provision-lan.sh
+++ scripts/provision-lan.sh
@@ -11,7 +11,7 @@ DEV_UUID=$(nmcli -c=no connection show | grep $INTERNAL_DEV | sed 's/^.* \([a-f0
 echo "It looks like the UUID is $DEV_UUID"
 echo $DEV_UUID | grep -q '^[a-f0-9-]\{36\}$' || exit 1
 
-INTERNAL_IP=$(ip route get 8.8.8.8 | sed -ne 's/.* via [0-9]\+\.\([0-9\.]\+\) .*/10.\1/p')
+INTERNAL_IP=$(ip route get 8.8.8.8 | sed -ne 's/.* src [0-9]\+\.\([0-9\.]\+\) .*/10.\1/p')
 echo "The internal IP will be $INTERNAL_IP"
 
 nmcli connection modify $DEV_UUID IPv4.address ${INTERNAL_IP}/8
