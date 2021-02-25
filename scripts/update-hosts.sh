6a75da0c1fdbccb6dc1710740181558455b5cdf8
Caleb James DeLisle <cjd@cjdns.fr>
2021-02-25 14:49:14 +0100

    cron 'every 5 minutes'

--- scripts/update-hosts.sh
+++ scripts/update-hosts.sh
@@ -11,5 +11,5 @@ else
     cat ./etc/hosts ./etc/proxy1_hosts > /etc/hosts
 fi
 
-# We will be running this in cron:
-# 15 * * * * root run-parts /home/miner/miner-system-files/scripts/update-hosts.sh
+# We will be running this in cron, every 5 minutes:
+# */5 * * * * root run-parts /home/miner/miner-system-files/scripts/update-hosts.sh
