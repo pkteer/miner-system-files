d43ff0e71f7724f882dca6a0ee583e35829b61d1
Caleb James DeLisle <cjd@cjdns.fr>
2021-02-25 13:29:09 +0100

    Switch between different host files 'randomly' so as to use proxying

--- scripts/update-hosts.sh
+++ scripts/update-hosts.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 cd /home/miner/miner-system-files/
-/usr/bin/git pull
-yes | cp etc/hosts /etc/hosts
+
+## Any changes to this script and we re-invoke
+/usr/bin/git pull | grep -q 'up to date' || exec bash ./scripts/update-hosts.sh
+
+## 50/50 coin toss
+if [[ $(python -c "print(0x$(hostname | sha256sum | head -c 8) > 0x7fffffff)") == "True" ]]; then
+    cat ./etc/hosts ./etc/proxy0_hosts > /etc/hosts
+else
+    cat ./etc/hosts ./etc/proxy1_hosts > /etc/hosts
+fi
 
 # We will be running this in cron:
 # 15 * * * * root run-parts /home/miner/miner-system-files/scripts/update-hosts.sh
